4dd83bdcc7a5b721b93213b6ef9e046b3adc0b05
Sabin Regmi <get2sabin@gmail.com>
2026-03-18 09:50:11 -0400

    Fix gallery mobile header overflow

--- makepad-gallery/src/ui/root.rs
+++ makepad-gallery/src/ui/root.rs
@@ -142,75 +142,83 @@ macro_rules! define_gallery_root {
                 header_bar := View{
                     width: Fill
                     height: Fit
-                    flow: Right
-                    align: Align{y: 0.5}
+                    flow: Down
                     padding: Inset{left: 16, right: 16, top: 12, bottom: 12}
-                    spacing: 12.0
+                    spacing: 10.0
                     draw_bg.color: (shad_theme.color_background)
 
-                    mobile_sidebar_toggle := View{
-                        width: 36
-                        height: 36
-                        flow: Overlay
-
-                        mobile_sidebar_menu_button := IconButtonMenu{
-                            width: 36
-                            height: 36
-                            draw_bg +: {
-                                color: #0000
-                                color_hover: (shad_theme.color_ghost_hover)
-                                color_down: (shad_theme.color_ghost_down)
-                                color_focus: (shad_theme.color_ghost_hover)
-                                border_size: 0.0
-                                border_radius: (shad_theme.radius)
-                                border_color: #0000
-                            }
-                            draw_icon.color: (shad_theme.color_primary)
-                        }
-
-                        mobile_sidebar_close_button := IconButtonX{
-                            visible: false
-                            width: 36
-                            height: 36
-                            draw_bg +: {
-                                color: #0000
-                                color_hover: (shad_theme.color_ghost_hover)
-                                color_down: (shad_theme.color_ghost_down)
-                                color_focus: (shad_theme.color_ghost_hover)
-                                border_size: 0.0
-                                border_radius: (shad_theme.radius)
-                                border_color: #0000
-                            }
-                            draw_icon.color: (shad_theme.color_primary)
-                        }
-                    }
-
-                    mobile_header_meta := View{
-                        width: Fit
+                    header_top_row := View{
+                        width: Fill
                         height: Fit
-                        flow: Down
-                        spacing: 2.0
+                        flow: Right
+                        align: Align{y: 0.5}
+                        spacing: 12.0
 
-                        mobile_header_caption := ShadSectionHeader{
-                            text: "Gallery"
+                        mobile_sidebar_toggle := View{
+                            width: 36
+                            height: 36
+                            flow: Overlay
+
+                            mobile_sidebar_menu_button := IconButtonMenu{
+                                width: 36
+                                height: 36
+                                draw_bg +: {
+                                    color: #0000
+                                    color_hover: (shad_theme.color_ghost_hover)
+                                    color_down: (shad_theme.color_ghost_down)
+                                    color_focus: (shad_theme.color_ghost_hover)
+                                    border_size: 0.0
+                                    border_radius: (shad_theme.radius)
+                                    border_color: #0000
+                                }
+                                draw_icon.color: (shad_theme.color_primary)
+                            }
+
+                            mobile_sidebar_close_button := IconButtonX{
+                                visible: false
+                                width: 36
+                                height: 36
+                                draw_bg +: {
+                                    color: #0000
+                                    color_hover: (shad_theme.color_ghost_hover)
+                                    color_down: (shad_theme.color_ghost_down)
+                                    color_focus: (shad_theme.color_ghost_hover)
+                                    border_size: 0.0
+                                    border_radius: (shad_theme.radius)
+                                    border_color: #0000
+                                }
+                                draw_icon.color: (shad_theme.color_primary)
+                            }
                         }
 
-                        mobile_page_label := ShadLabel{
-                            text: "Components"
-                            draw_text.text_style.font_size: 12
+                        mobile_header_meta := View{
+                            width: Fit
+                            height: Fit
+                            flow: Down
+                            spacing: 2.0
+
+                            mobile_header_caption := ShadSectionHeader{
+                                text: "Gallery"
+                            }
+
+                            mobile_page_label := ShadLabel{
+                                text: "Components"
+                                draw_text.text_style.font_size: 12
+                            }
                         }
+
+                        View{
+                            width: Fill
+                            height: Fit
+                        }
+
+                        mobile_theme_toggle := mod.widgets.GalleryThemeToggle{}
                     }
 
                     mobile_command_palette_trigger := ShadButtonGhost{
+                        width: Fill
                         text: "Search components"
                     }
-
-                    View{
-                        width: Fill
-                        height: Fit
-                    }
-
-                    mobile_theme_toggle := mod.widgets.GalleryThemeToggle{}
                 }
 
                 ShadSeparator{}
