13a4fccbcd2fa20fb9fb1c43ea182fd0ec797b2e
Sabin Regmi <get2sabin@gmail.com>
2026-03-23 12:53:54 -0400

    Keep command palette results out of tab order

--- makepad-gallery/src/ui/command_palette.rs
+++ makepad-gallery/src/ui/command_palette.rs
@@ -111,6 +111,7 @@ script_mod! {
             button := ShadButtonGhost{
                 width: Fill
                 height: 36
+                grab_key_focus: false
                 padding: Inset{left: 0, right: 0, top: 0, bottom: 0}
                 align: Align{x: 0.0, y: 0.5}
                 text: "Command"
