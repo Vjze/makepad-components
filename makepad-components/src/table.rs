baa7757a330672ecc35f1884da89a5a9d0cf8fe0
Vjze <99671889+Vjze@users.noreply.github.com>
2026-04-05 19:08:15 +0800

    Change table height from fixed to fill

--- makepad-components/src/table.rs
+++ makepad-components/src/table.rs
@@ -100,7 +100,7 @@ script_mod! {
                     header := mod.widgets.ShadTableHeaderView{}
                     list := PortalList{
                         width: Fill
-                        height: 208
+                        height: Fill
                         flow: Down
                         max_pull_down: 0.0
                         capture_overload: true
